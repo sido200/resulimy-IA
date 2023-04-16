6b04d6c60bd51684c8c95a2b3eaff3ae97f13ecf
saliiimm <salimghalem40@gmmail.com>
2023-04-16 05:23:11 +0100

    nav-rigl

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-const cat=document.querySelector(".cat") 
-const cat_i=document.querySelector(".cat i") 
-const sous_cat=document.querySelector(".sous-cat") 
-cat.addEventListener('click',()=>{
-   if(cat_i.classList.value==="fa-solid fa-caret-right"){
-    cat_i.classList.remove("fa-caret-right")
-    cat_i.classList.add("fa-caret-down")
-    sous_cat.style.display="flex"
-   }
-   else{
-    cat_i.classList.remove("fa-caret-down")
-    cat_i.classList.add("fa-caret-right")
-    sous_cat.style.display="none"
-  
-   }
-})
-let btn=document.querySelector('.fa-bars')
-let items=document.querySelector('.ul')
-btn.addEventListener('click',function(){
-    items.classList.toggle('active-barre')
-console.log("hi");
-})
+const cat = document.querySelector(".cat");
+const cat_i = document.querySelector(".cat i");
+const sous_cat = document.querySelector(".sous-cat");
+cat.addEventListener("click", () => {
+  if (cat_i.classList.value === "fa-solid fa-caret-right") {
+    cat_i.classList.remove("fa-caret-right");
+    cat_i.classList.add("fa-caret-down");
+    sous_cat.style.display = "flex";
+  } else {
+    cat_i.classList.remove("fa-caret-down");
+    cat_i.classList.add("fa-caret-right");
+    sous_cat.style.display = "none";
+  }
+});
+let btn = document.querySelector(".fa-bars");
+let items = document.querySelector(".ul");
+const itemli = document.querySelectorAll(".ul a");
+btn.addEventListener("click", function () {
+  items.classList.toggle("active-barre");
+  console.log("hi");
+});
+for (i = 0; i < itemli.length; i++) {
+  itemli[i].addEventListener("click", function () {
+    items.classList.remove("active-barre");
+  });
+}
